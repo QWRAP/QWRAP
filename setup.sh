af2d89a182aad1b47a3c82013b2db4ad91681f93
Ranjit Kumar <rkumar@uab.edu>
2014-07-23 14:17:17 -0500

    regular update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
-echo "This command will find the absolute path of QWRAP and add it to file ~/.bashrc"
-echo -e "export PATH=\${PATH}:$PWD/" >> ~/.bashrc
+echo "export PATH=\${PATH}:$PWD/" >> ~/.bashrc
 source ~/.bashrc
 
-echo "Done"
+echo "Setup Done"
 
