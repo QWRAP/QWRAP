ba49a6f02f3c52c44054890297398b884fc38b23
Denilson Figueiredo de Sá <denilsonsa@gmail.com>
2016-06-03 18:50:37 -0300

    `convert_biom.py` to `biom convert`
    Also, from version 1.3 to 2.0, they refactored the code and `--biom-to-classic-table` turned into `--to-tsv` (I belive), but it was not mentioned in the [ChangeLog](https://github.com/biocore/biom-format/blob/master/ChangeLog.md). :(

--- microbiome-workflow1.sh
+++ microbiome-workflow1.sh
@@ -105,7 +105,7 @@ echo -e "">>script.sh
 echo '#------------------ Convert BIOM file to TXT file----------------'>>script.sh
 echo -e 'echo "Time: `date`"' >>script.sh
 echo -e 'echo "Running: Converting BIOM file to TXT file"'>>script.sh
-echo 'convert_biom.py -i otu_table_org.biom -o otu_table_org.txt -b --header_key taxonomy --output_metadata_id "ConsensusLineage"'>>script.sh
+echo 'biom convert -i otu_table_org.biom -o otu_table_org.txt --to-tsv --header-key taxonomy --output-metadata-id "ConsensusLineage"'>>script.sh
 echo -e "">>script.sh
 
 
